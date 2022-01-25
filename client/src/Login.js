4bb551a6b95a352799fc19efb1df02cc2e455b90
OptimisticShibe <britishdragons123@gmail.com>
2022-01-24 22:49:28 -0600

    tried setting up serverless

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import SpotifyWebApi from "spotify-web-api-node";
 
 const SCOPES = ["user-read-private", "user-top-read"];
 const CLIENT_ID = "f655ecf166914d6b9ecf6d7abcc91c52";
@@ -8,6 +9,15 @@ const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMETER);
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=${SCOPES_URL_PARAM}`;
 
 export default function Login() {
+  const handleLogin = () => {
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: process.env.REDIRECT_URI,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    });
+    const authorizeURL = spotifyApi.createAuthorizeURL();
+  };
+
   return (
     <div style={{ backgroundColor: "#282c34" }}>
       <Container className="d-flex justify-content-center align-items-center flex-column" style={{ minHeight: "100vh", color: "white" }}>
